04c39fbd5b216be2cf3e3c801445485a50ea196c
Pontiac76 <pontiac76@gmail.com>
2022-12-18 03:09:54 -0500

    Update kickmount.sh

--- NetworkMount/kickmount.sh
+++ NetworkMount/kickmount.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-#This script can sit in the /root directory.
+# This script can sit in the /root directory.
 
+# Perform the remount using already provided scripts
 /media/fat/Scripts/cifs_umount.sh
 /media/fat/Scripts/cifs_mount.sh
 
+# Go into the games directory and hunt down all the _Network files and delete them
 cd /media/fat/games &&  find . | grep _Network | xargs rm
-for p in `find . -maxdepth 1 -type d | grep -v " " | grep -v "\.$" `
+
+# Go into the games directory and get a list of the directories for each core
+cd /media/fat/games && for p in `find . -maxdepth 1 -type d | grep -v " " | grep -v "\.$" `
 do
+        # If the directory exists on the NAS, link the directory and report verbosely.
         if [ -d /media/nas/${p} ];
         then
                 ln -sv /media/nas/${p} ${p}/_Network;
